e78b03483d3804e13db88ab037482a77dbe51cb8
aparkening <aaron.parkening@gmail.com>
2019-05-31 16:18:04 -0400

    Done. Students have been scraped and output from CLI.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,64 @@
+require 'nokogiri'
 require 'open-uri'
-require 'pry'
-
 class Scraper
 
+  # Return array of student hashes 
   def self.scrape_index_page(index_url)
-    
+    students = []
+
+    # Store html, get profiles
+    doc = Nokogiri::HTML(open(index_url))
+    student_profiles = doc.css("div.roster-cards-container .student-card")
+
+    # Iterate through profiles, create student hashes
+    student_profiles.collect do |student| 
+      students <<
+      {
+        :name => student.css(".card-text-container h4.student-name").text, 
+        :location => student.css(".card-text-container p.student-location").text, 
+        :profile_url => student.css("a").attr("href").value
+      }
+    end
+    return students
   end
 
+  # Return hash describing individual student
   def self.scrape_profile_page(profile_url)
+    profile = {}
+
+    # Store html document
+    doc = Nokogiri::HTML(open(profile_url))
+
+    # Store main vitals
+    vitals_container = doc.css("div.profile .vitals-container").first
+
+    # Set social urls if they exist
+    twitter = ""
+    linkedin = ""
+    github = ""
+    blog = ""
+    links = vitals_container.css(".social-icon-container a").each do |link| 
+      if link.attr("href").include?("twitter.com") 
+        profile[:twitter] = link.attr("href")
+      elsif link.attr("href").include?("linkedin.com") 
+        profile[:linkedin] = link.attr("href")
+      elsif  link.attr("href").include?("github.com") 
+        profile[:github] = link.attr("href")
+      elsif  link.attr("href").include?("facebook.com") 
+        facebook = link.attr("href")
+      else 
+        profile[:blog] = link.attr("href")
+      end
+    end
+
+    # # Grab twitter url if it exists
+    # twitter = vitals_container.css(".social-icon-container a[href*='twitter.com']").attr("href").value if vitals_container.css(".social-icon-container a[href*='twitter.com']").first 
+
+    # Get profile quote and bio if they exist
+    profile[:profile_quote] = vitals_container.css(".vitals-text-container .profile-quote").text if profile[:profile_quote] = vitals_container.css(".vitals-text-container .profile-quote").first
     
+    profile[:bio] = doc.css("div.profile .details-container p").first.text if profile[:bio] = doc.css("div.profile .details-container p").first
+
+    profile
   end
-
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,35 @@
+require 'pry'
 class Student
 
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
 
   @@all = []
 
-  def initialize(student_hash)
-    
+  # Initialize with send
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+    @@all << self
   end
 
+  # Create student instance from each hash of input array
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student|
+      student = Student.new({
+        :name => student[:name],
+        :location => student[:location],
+        :profile_url => student[:profile_url]  
+      })
+    end
   end
 
+  # Add student attributes from profile page hash
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each {|key, value| self.send(("#{key}="), value)}
+    self
   end
 
+  # @all reader
   def self.all
-    
+    @@all
   end
 end
-
